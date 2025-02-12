a8a5bed22d5932202850353e4a3c693841836dcc
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-02-12 16:07:26 +0530

    home about

--- apps/portfolio/components/Hero.tsx
+++ apps/portfolio/components/Hero.tsx
@@ -14,6 +14,10 @@ const Hero = () => {
         />
         <Spotlight
           className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="blue"
+        />
+         <Spotlight
+          className="h-[80vh] w-[50vw] top-40 left-full"
           fill="purple"
         />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
@@ -33,7 +37,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Dynamic Web Magic with <span className="text-purple"> Avinash Suthar </span>
           </p>
 
           {/**
@@ -47,7 +51,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Avinash, a Full Stack Developer from India.
+            Hi! I<span className="text-purple">&apos;</span>m <span className="text-purple"> Avinash Suthar,</span> a Full Stack Developer from India.
           </p>
 
           <a href="#about" title="Home">
